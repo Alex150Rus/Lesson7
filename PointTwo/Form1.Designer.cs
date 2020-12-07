a1eab08bde9353c3c4d4d7ca3525450e801f3adc
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2020-12-08 00:10:28 +0300

    Point two is ready

---
+++ PointTwo/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿
+namespace PointTwo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.userNumber = new System.Windows.Forms.TextBox();
+            this.lblNumber = new System.Windows.Forms.Label();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.lblTip = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // userNumber
+            // 
+            this.userNumber.Location = new System.Drawing.Point(409, 66);
+            this.userNumber.Name = "userNumber";
+            this.userNumber.Size = new System.Drawing.Size(100, 20);
+            this.userNumber.TabIndex = 0;
+            this.userNumber.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.userNumber_Input);
+            // 
+            // lblNumber
+            // 
+            this.lblNumber.AutoSize = true;
+            this.lblNumber.Location = new System.Drawing.Point(183, 73);
+            this.lblNumber.Name = "lblNumber";
+            this.lblNumber.Size = new System.Drawing.Size(220, 13);
+            this.lblNumber.TabIndex = 1;
+            this.lblNumber.Text = "Компьютер загадал число от нуля до 100:";
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(51, 62);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(75, 23);
+            this.btnPlay.TabIndex = 2;
+            this.btnPlay.Text = "Играть";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // lblTip
+            // 
+            this.lblTip.AutoSize = true;
+            this.lblTip.Location = new System.Drawing.Point(183, 105);
+            this.lblTip.Name = "lblTip";
+            this.lblTip.Size = new System.Drawing.Size(69, 13);
+            this.lblTip.TabIndex = 3;
+            this.lblTip.Text = "Подсказка: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblTip);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.lblNumber);
+            this.Controls.Add(this.userNumber);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox userNumber;
+        private System.Windows.Forms.Label lblNumber;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Label lblTip;
+    }
+}
+
---
+++ PointTwo/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PointTwo
+{
+    /// <summary>
+    /// Используя Windows Forms, разработать игру «Угадай число». Компьютер загадывает число от 1 до 100, 
+    /// а человек пытается его угадать за минимальное число попыток. Компьютер говорит, больше или меньше загаданное число введенного.  
+    /// a) Для ввода данных от человека используется элемент TextBox;
+    /// б) ** Реализовать отдельную форму c TextBox для ввода числа.
+    /// </summary>
+    public partial class Form1 : Form
+    {
+        int compNumber;
+        bool playing = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Обработчик события клика на кнопку Играть
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            SetCompNumber();
+            lblTip.Text = "Подсказка:";
+            userNumber.ReadOnly = false;
+            userNumber.Text = "";
+            playing = true;
+            btnPlay.BackColor = Color.Green;           
+        }
+
+        /// <summary>
+        /// Метод для обработки ввода пользователя после нажатия Enter
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void userNumber_Input(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == Convert.ToChar(Keys.Enter))
+            {
+                int result;
+                int.TryParse(userNumber.Text, out result);
+                if (result > 0 && playing)
+                {
+                    string tip;
+                    if (result < compNumber) tip = "Число компьютера больше";
+                    else if (result > compNumber) tip = "Число компьютера меньше";
+                    else
+                    {
+                        tip = "Вы угадали";
+                        userNumber.ReadOnly = true;
+                        playing = false;
+                        btnPlay.BackColor = Color.Red;
+
+                    }
+                    lblTip.Text = tip;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Метод отвечает за загадывание номера
+        /// </summary>
+        private void SetCompNumber()
+        {
+            Random n = new Random();
+            compNumber = n.Next(0, 100);
+        }
+
+    }
+}
